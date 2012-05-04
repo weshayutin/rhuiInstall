2f64b91aaf4b140aa390c5a6c2a8eb97792bf067
wes hayutin <weshayutin@gmail.com>
2012-05-03 20:08:41 -0400

    minor updates, playing w/ shell script

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ from fabric.api import env, run, put, settings
 
 parser = optparse.OptionParser()
 parser.add_option('-n', '--name', help='name the environment')
-parser.add_option('-e', '--env', help='pick environment: ec2,local',
-     dest='env', type='string')
+parser.add_option('-e', '--install_env', help='pick environment: ec2,local',
+     dest='install_env', type='string')
 
 
 (opts, args) = parser.parse_args()
 
 # Making sure all mandatory options appeared.
-if not opts.env:
+if not opts.install_env:
     print "requires an environment"
     parser.print_help()
     exit(-1)
@@ -29,33 +29,37 @@ cfg = INIConfig(open('config'))
 
 
 if __name__ == '__main__':
+    print(opts.install_env)
+    if opts.install_env == 'ec2':
+        ## start an instance
+        e = ec2h(cfg.EC2.region, cfg.EC2.username, cfg.EC2.password)
+        myConn = e.getConnection()
+        dict = {}
+        #rhui = ['rhua', 'cds1', 'cds2', 'proxy', 'client']
+        rhui = ['rhua', 'cds1']
+        for i in rhui:
+            thisInstance = e.startInstance(cfg.EC2.ami_id, cfg.EC2.east_keyName,
+                myConn, 'm1.large')
+            instanceDetails = thisInstance.__dict__
+            this_hostname = instanceDetails['public_dns_name']
+            print(this_hostname)
+            dict[i] = thisInstance
 
-    print cfg.EC2.ec2_username
+        ## wait for instance and check ssh
+        print dict.keys()
+        print('wait for instances to boot for 130 seconds')
+        time.sleep(130)
+        print('done waiting for instances to boot')
+        for i in dict.keys():
+            instance = dict[i].__dict__
+            hostname = instance['public_dns_name'].encode('ascii')
+            env.host_string = hostname
+            env.user = 'root'
+            env.key_filename = cfg.EC2.east_key
+            run('hostname')
 
-    ## start an instance
-    e = ec2h(cfg.EC2.region, cfg.EC2.username, cfg.EC2.password)
-    myConn = e.getConnection()
-    dict = {}
-    #rhui = ['rhua', 'cds1', 'cds2', 'proxy', 'client']
-    rhui = ['rhua', 'cds1']
-    for i in rhui:
-        thisInstance = e.startInstance(cfg.EC2.ami_id, cfg.EC2.east_keyName,
-            myConn, 'm1.large')
-        instanceDetails = thisInstance.__dict__
-        this_hostname = instanceDetails['public_dns_name']
-        print(this_hostname)
-        dict[i] = thisInstance
+    elif opts.install_env == 'local':
+        print('in local')
 
-    ## wait for instance and check ssh
-    print dict.keys()
-    print('wait for instances to boot for 130 seconds')
-    time.sleep(130)
-    print('done waiting for instances to boot')
-    for i in dict.keys():
-        instance = dict[i].__dict__
-        hostname = instance['public_dns_name'].encode('ascii')
-        env.host_string = hostname
-        env.user = 'root'
-        env.key_filename = cfg.EC2.east_key
-        run('hostname')
+
 
